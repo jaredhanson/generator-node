43d76289e0aab8beb738e983aa4e026b992dc548
Jared Hanson <jaredhanson@gmail.com>
2013-11-12 12:32:21 -0800

    Preserve existing values in package.json when generating.

--- app/index.js
+++ app/index.js
@@ -1,10 +1,18 @@
 var generator = require('yeoman-generator')
   , path = require('path')
-  , util = require('util');
+  , fs = require('fs')
+  , util = require('util')
+  , existsSync = fs.existsSync || path.existsSync; // node <=0.6
 
 
 function NodeGenerator(args, options) {
   generator.Base.apply(this, arguments);
+  
+  if (existsSync('package.json')) {
+    try {
+      this.pkg = JSON.parse(fs.readFileSync('package.json'));
+    } catch (_) {}
+  }
 }
 
 util.inherits(NodeGenerator, generator.Base);
@@ -15,27 +23,27 @@ NodeGenerator.prototype.doPrompt = function() {
   var prompts = [{
     name: 'name',
     message: 'Name',
-    default: path.basename(process.cwd())
+    default: this.pkg && this.pkg.name ? this.pkg.name : path.basename(process.cwd())
   }, {
     name: 'version',
     message: 'Version',
-    default: '0.0.0'
+    default: this.pkg && this.pkg.version ? this.pkg.version : '0.0.0'
   }, {
     name: 'description',
     message: 'Description',
-    default: ''
+    default: this.pkg && this.pkg.description ? this.pkg.description : ''
   }, {
     name: 'authorName',
     message: 'Author Name',
-    default: ''
+    default: this.pkg && this.pkg.author && this.pkg.author.name ? this.pkg.author.name : ''
   }, {
     name: 'authorEmail',
     message: 'Author Email',
-    default: ''
+    default: this.pkg && this.pkg.author && this.pkg.author.email ? this.pkg.author.email : ''
   }, {
     name: 'authorUrl',
     message: 'Author URL',
-    default: ''
+    default: this.pkg && this.pkg.author && this.pkg.author.url ? this.pkg.author.url : ''
   }];
   
   this.prompt(prompts, function(props) {
