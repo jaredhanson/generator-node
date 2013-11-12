b030a17fcc93e2239523cbc37981bed5e4dd4447
Jared Hanson <jaredhanson@gmail.com>
2013-11-11 20:36:30 -0800

    Initial commit.

---
+++ app/index.js
@@ -0,0 +1,47 @@
+var generator = require('yeoman-generator')
+  , path = require('path')
+  , util = require('util');
+
+
+function NodeGenerator(args, options) {
+  generator.Base.apply(this, arguments);
+}
+
+util.inherits(NodeGenerator, generator.Base);
+
+NodeGenerator.prototype.doPrompt = function() {
+  var done = this.async();
+
+  var prompts = [{
+    name: 'name',
+    message: 'Name',
+    default: path.basename(process.cwd())
+  }, {
+    name: 'version',
+    message: 'Version',
+    default: '0.0.0'
+  }, {
+    name: 'description',
+    message: 'Description',
+    default: ''
+  }];
+  
+  this.prompt(prompts, function(props) {
+    props.description = props.description || 'TODO: No description specified';
+    
+    this.props = props;
+    done();
+  }.bind(this));
+};
+
+NodeGenerator.prototype.doGenerate = function() {
+  this.template('package.json', 'package.json');
+  this.mkdir('lib');
+  
+  this.mkdir('support');
+  this.mkdir('support/mk');
+  
+  this.copy('_gitignore', '.gitignore');
+}
+
+module.exports = NodeGenerator;
