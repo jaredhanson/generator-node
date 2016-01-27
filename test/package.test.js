82f76fb81adad37cc6fbfe0e7425fa10bd561328
Jared Hanson <jaredhanson@gmail.com>
2016-01-26 19:44:02 -0800

    Start adding test cases.

--- test/package.test.js
+++ test/package.test.js
@@ -1,11 +1,50 @@
 /* global describe, it, expect */
 
-var generator = require('..');
+var generator = require('../app');
+var assert = require('yeoman-assert');
+var helpers = require('yeoman-test');
+var path = require('path');
 
-describe('generator-node', function() {
+
+describe('node:app', function() {
   
   it('should export constructor', function() {
     expect(generator).to.be.a('function');
+  });
+  
+  describe('defaults', function () {
+    before(function(done) {
+      helpers.run(path.join( __dirname, '../app'))
+        .withPrompts({ coffee: false }) // Mock the prompt answers
+        .on('ready', function(generator) {
+        })
+        .on('end', function() {
+          console.log(process.cwd());
+          done();
+        });
+    });
+    
+    it('creates files', function () {
+      var expected = [
+        '.gitignore',
+        '.jshintrc',
+        '.npmignore',
+        '.travis.yml',
+        'LICENSE',
+        'Makefile',
+        'package.json'
+      ];
+
+      assert.file(expected);
+    });
+
+    it('generate a router.js file', function () {
+      // assert the file exist
+      // assert the file uses AMD definition
+    });
+
+    it('generate a view file');
+    it('generate a base controller');
   });
   
 });
